76abcfabb5c80f5c716ddda82d4be25e3b9e21a1
yanmiao99 <503084944@qq.com>
2022-08-28 12:12:01 +0800

    feat : 连接 mysql 调试成功

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const app = express()
 const routerTest = require('./routes/test.js')
 const router = require("./routes/index.js")
 const {port} = require("./config/index.js")
+const cors = require("cors")
 
 // 配置全局中间件 ( 中间键必须配置在路由的前面, 否则不生效 )
 app.use(express.json())  // 解析 json 数据
 app.use(express.urlencoded({extended: false})) // 解析 x-www-form-urlencoded 数据
+app.use(cors()) // 解决跨域
 
 // 路由
 app.use('/test', routerTest);
@@ -23,7 +25,7 @@ app.use("/api", router)
 app.use(express.static('public'))
 
 app.listen(port, () => {
-    console.log(`当前启动的端口为 :  ${port}`)
+    console.log(`✅ - 当前启动的端口为 :  ${port}`)
 })
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,4 +2,20 @@ let express = require('express');
 let router = express.Router();
 let connection = require('../db/index.js')
 
+// get 查询数据库的所有数据
+
+router.get("/list", (req, res) => {
+    connection.query("SELECT * FROM `user`", (err, data) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(data)
+    })
+})
+
+
 module.exports = router;
+
+
+
+
