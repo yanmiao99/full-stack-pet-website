133e126880e8ac40ce9ad9e1da0703532ba68fbb
yanmiao99 <503084944@qq.com>
2022-09-02 16:21:03 +0800

    feat : 路由鉴权 JWT

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const routerTestToken = require('./routes/test-token.js')
 const router = require('./routes/index.js')
 const { port, secretKey } = require('./config/index.js')
 const cors = require('cors')
-// const { expressjwt } = require('express-jwt')
+const { expressjwt } = require('express-jwt')
 
 // 配置全局中间件 ( 中间键必须配置在路由的前面, 否则不生效 )
 app.use(express.json()) // 解析 json 数据
@@ -15,22 +15,22 @@ app.use(cors()) // 解决跨域
 
 // expressjwt 用来解析 token 的中间件
 // .unless 用来指定什么接口不需要访问权限
-// app.use(expressjwt({
-//   credentialsRequired: true, // false：不校验
-//   secret: secretKey, // 加密秘钥
-//   algorithms: ['HS256'] // 6.0 版本以后, 必须配置
-// }).unless({
-//   path: [
-//     // 添加不需要token验证的路由 , 也可以使用正则
-//     /^\/test\/.*/,
-//     '/testToken/login'
-//   ]
-// }))
+app.use(expressjwt({
+  credentialsRequired: true, // false：不校验
+  secret: secretKey, // 加密秘钥
+  algorithms: ['HS256'] // 6.0 版本以后, 必须配置
+}).unless({
+  path: [
+    // 添加不需要token验证的路由 , 也可以使用正则
+    /^\/api\/.*/
+    // '/testToken/login'
+  ]
+}))
 
 // 路由
-app.use('/test', routerTest)
-app.use('/test/db', routerTestDB)
-app.use('/testToken', routerTestToken)
+// app.use('/test', routerTest)
+// app.use('/test/db', routerTestDB)
+// app.use('/testToken', routerTestToken)
 app.use('/api', router)
 
 // 静态资源
@@ -44,15 +44,15 @@ app.use('/api', router)
 app.use(express.static('public'))
 
 // 异常捕获的中间件 ( 需要放在所有路由的最后面 )
-// app.use((err, req, res, next) => {
-//   // Token 解析失败导致错误
-//   if (err.name === 'UnauthorizedError') {
-//     return res.send({ code: 401, msg: '无效的token' })
-//   }
-//
-//   // 其他原员导致的错误
-//   res.send({ code: 500, msg: '未知错误' })
-// })
+app.use((err, req, res, next) => {
+  // Token 解析失败导致错误
+  if (err.name === 'UnauthorizedError') {
+    return res.send({ code: 401, msg: '无效的token' })
+  }
+
+  // 其他原员导致的错误
+  res.send({ code: 500, msg: '未知错误' })
+})
 
 app.listen(port, () => {
   console.log(`✅ - 当前启动的端口为 :  ${port}`)
